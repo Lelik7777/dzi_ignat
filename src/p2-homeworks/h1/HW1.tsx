b5365eafe5cb1edf5f2270e589365bdfa9e2f7f9
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-10-21 11:53:59 +0300

    dz-01 finish

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import Message from './Message';
+import MessageHW1 from './Message';
 import s from './Message.module.css';
+import logo1 from './img/avatar_1634804341103.png';
+import logo2 from './img/avatar_1634803710085.png';
+import logo3 from './img/avatar_1634803732159.png';
+import logo4 from './img/avatar_1634803761499.png';
+
+function getName() {
+    const names = ['Bob', 'Tom', 'Alex', 'Nick'];
+    return names[Math.trunc(Math.random() * names.length)];
+}
+function getAvatar() {
+    const ava=[`${logo1}`,`${logo2}`,`${logo3}`,`${logo4}`];
+    return ava[Math.trunc(Math.random()*ava.length)];
+}
 export type MessageDateType = {
     avatar: string;
     name: string;
     message: string;
     time: string;
 }
+
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+   /* avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',*/
+    avatar:`${getAvatar()}`,
+    name: `${getName()}`,
+    message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem deserunt dolores et facilis fuga ipsam laudantium ',
+    time: `${new Date().getHours()}:${new Date().getMinutes()}`
 }
 
 function HW1() {
@@ -20,7 +35,7 @@ function HW1() {
             <hr/>
             homeworks 1
 
-            <Message
+            <MessageHW1
                 avatar={messageData.avatar}
                 name={messageData.name}
                 message={messageData.message}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -3,11 +3,16 @@ import {MessageDateType} from './HW1';
 import s from './Message.module.css';
 
 
-function Message(props:MessageDateType) {
+function Message(props: MessageDateType) {
     return (
         <div className={s.message}>
             <img src={props.avatar} alt="avatar"/>
-            <div className={s.bodyMes}>sadf</div>
+            <div className={s.corner}></div>
+            <div className={s.bodyMes}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
